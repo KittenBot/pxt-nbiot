258ad35669d86404926a19a35f4c192dcf426ae1
Riven <4771215@qq.com>
2019-09-11 18:19:01 +0800

    mqtt works, mostly~

--- main.ts
+++ main.ts
@@ -32,17 +32,38 @@ namespace nbiot {
     type EvtNum = (data: number) => void;
     type EvtDict = (topic: string, data: string) => void;
 
+    let netConn: EvtAct = null;
+    let mqttOpen: EvtAct = null;
+
     let lastCmd: string;
-    let cmdCache: string[] = []
     let topicCB: StringMessageHandler[] = []
+    let isConn: boolean = false;
 
     serial.onDataReceived('\n', function () {
         let a = serial.readString()
+        if (a.charCodeAt(0) > 0x1f) {
+            console.log(">>" + a)
+        }
         if (a.charAt(0) == '+') {
-            let b = a.slice(1).split(":")
+            let b = a.slice(1, a.length - 1).split(":")
             let cmd = b[0]
             let params = b[1].split(',')
-            console.log(">>" + cmd + "/"+ params.length() + "/" + params.join("#"))
+            if (cmd == "CEREG") { // eps registered
+                if ((params.length == 2 && params[1].charAt(0) == '1') ||
+                    (params.length == 1 && params[0].charAt(0) == '1')
+                ) {
+                    isConn = true;
+                    if (netConn) netConn()
+                }
+            } else if (cmd == "MQTTOPEN") {
+                if (mqttOpen) mqttOpen()
+            } else if (cmd == "MQTTPUBLISH") {
+                for (let i = 0; i < topicCB.length; i++) {
+                    if (topicCB[i].topicName == params[4]) {
+                        topicCB[i].fn(params[6])
+                    }
+                }
+            }
         }
 
     })
@@ -51,16 +72,18 @@ namespace nbiot {
         let str = "AT+" + op + "=" + cmd
         lastCmd = op
         serial.writeLine(str)
+        basic.pause(200)
     }
 
     function cellInit() {
         // cell init sequence
-        sendAtCmd("COPS", `1, 2, "46000"`)
-        basic.pause(200)
-        sendAtCmd("CSCON", "1")
-        basic.pause(200)
-        sendAtCmd("CEREG", "1")
-
+        serial.writeLine("AT+NRB")
+        basic.pause(8000) // wait reboot finish
+        sendAtCmd("COPS", `0`)
+        sendAtCmd("CGATT", `1`)
+        sendAtCmd("CSCON", `1`)
+        sendAtCmd("CEREG", `1`)
+        serial.writeLine("AT+CSQ")
     }
 
     /**
@@ -85,6 +108,13 @@ namespace nbiot {
         cellInit();
     }
 
+
+    /**
+     * Mqtt host config
+     * @param host Mqtt server ip or address; eg: iot.kittenbot.cn
+     * @param port host port; eg: 1883
+     * @param id Client Id; eg: node01
+    */
     //% blockId=nbiot_mqttconfig block="Mqtt Config Host %host|Port %port|ClientID %id|User %user|Pass %pass"
     //% weight=100
     export function nbiot_config(host: string, port: number, id: string, user?: string, pass?: string): void {
@@ -99,9 +129,13 @@ namespace nbiot {
     }
 
     //% blockId=nbiot_mqttsub block="Mqtt Subscribe %topic"
-    export function nbiot_mqttsub(topic: string, handler: (str: string) => void) {
-        sendAtCmd("MQTTSUB", `"${topic}", 1`)
-        cmdCache.push(cmd)        let topicHandler = new StringMessageHandler()
+    export function nbiot_mqttsub(topic: string) {
+        sendAtCmd("MQTTSUB", `"${topic}",1`)
+    }
+
+    //% blockId=nbiot_mqtt_onmsg block="on Mqtt Msg topic%topic"
+    export function nbiot_mqtt_onmsg(topic: string, handler: (str: string) => void) {
+        let topicHandler = new StringMessageHandler()
         topicHandler.fn = handler
         topicHandler.topicName = topic
         topicCB.push(topicHandler)
@@ -111,6 +145,28 @@ namespace nbiot {
     export function nbiot_mqttpub(topic: string, data: string, qos?: number) {
         qos = qos ? 1 : 0;
         sendAtCmd("MQTTPUB", `"${topic}",${qos},0,0,0,"${data}"`)
+    }
+
+    //% blockId=nbiot_checkcon block="Check Connection"
+    export function nbiot_checkcon() {
+        serial.writeLine("AT+CSCON?")
+        basic.pause(200)
+        serial.writeLine("AT+CGATT?")
+        basic.pause(200)
+        serial.writeLine("AT+CEREG?")
+        basic.pause(200)
+    }
+
+    //% blockId=on_net_connected block="on Net Connected"
+    //% weight=50
+    export function on_net_connected(handler: () => void): void {
+        netConn = handler;
+    }
+
+    //% blockId=on_mqtt_open block="on Mqtt Open"
+    //% weight=50
+    export function on_mqtt_open(handler: () => void): void {
+        mqttOpen = handler;
     }
 
 }
